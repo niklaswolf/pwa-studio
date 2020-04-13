fbfe3e6d07d80da46b83e14b59424ff5d97d2768
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-04-13 14:48:07 -0700

    Update radios

--- packages/venia-styleguide/src/components/RadioInput/RadioInput.js
+++ packages/venia-styleguide/src/components/RadioInput/RadioInput.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Circle } from 'react-feather';
 
 import classes from './RadioInput.css';
 
 const RadioInput = props => {
-    const { children, value, ...restProps } = props;
+    const { children, initialChecked, value, ...restProps } = props;
 
     return (
         <label className={classes.root}>
             <input
                 {...restProps}
                 className={classes.input}
+                defaultChecked={initialChecked}
                 type="radio"
                 value={value}
             />
+            <span className={classes.icon}>
+                <Circle />
+            </span>
             <span className={classes.label}>{children}</span>
         </label>
     );
